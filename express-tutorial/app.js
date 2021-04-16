42af17785077bb0dabae2f40d8e69b15e4e01d01
PadoSensei <padraigo.donoghue@gmail.com>
2021-04-16 12:54:32 +0100

    Refactor File Structure

---
+++ express-tutorial/app.js
@@ -0,0 +1,23 @@
+const http = require('http')
+
+
+const server = http.createServer((req, res) => {
+  console.log('user hit the server')
+  const url = req.url;
+  if(url === '/') {
+    
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>The Home Page</h1>')
+    res.end()
+  } else if(url === '/about'){
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>The About Page /h1>')
+    res.end()
+  } else {
+    res.writeHead(404, {'content-type': 'text/html'})
+    res.write('<h1>Nothing Found</h1>')
+    res.end()
+  }
+})
+
+server.listen(5000)
